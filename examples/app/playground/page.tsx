99f9f08c2d6b72fdbd4fb59579fd4b5d578591ef
Rob Gordon <rchristopher.gordon@gmail.com>
2023-02-10 13:27:37 -0500

    add playground

--- examples/app/playground/page.tsx
+++ examples/app/playground/page.tsx
@@ -1,10 +1,11 @@
 import { Editor } from "@/components/Editor";
+import { PlaygroundState } from "@/components/PlaygroundState";
 
 export default function Page() {
   return (
     <div className="min-h-screen p-12 grid gap-3 content-start">
       <h1 className="text-3xl font-bold">Playground</h1>
-      <Editor h={500} className="shadow w-12" />
+      <PlaygroundState />
     </div>
   );
 }
