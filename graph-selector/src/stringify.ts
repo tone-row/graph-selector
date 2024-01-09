da224436fbe08613bd9149a3b71a4f473a327650
Rob Gordon <rob.gordon@tone-row.com>
2024-01-09 15:39:13 -0500

    Cleanup

--- graph-selector/src/stringify.ts
+++ graph-selector/src/stringify.ts
@@ -1,62 +1,73 @@
 import { Graph } from "./types";
 
+type StringifyOptions = {
+  /**
+   * Whether to compact the output by removing newlines and spaces
+   */
+  compact?: boolean;
+};
+
+const defaultOptions: StringifyOptions = {
+  compact: false,
+};
+
 /**
  * Convets a graph to graph-selector DSL
  */
-export function stringify(graph: Graph) {
+export function stringify(graph: Graph, _options: StringifyOptions = defaultOptions) {
   const lines: string[] = [];
-  for (const node of graph.nodes) {
-    const { label: _label, id: _id, classes: _classes, ..._data } = node.data;
 
+  // In compact mode, we will store where edges should be added
+
+  // Loop over nodes
+  for (const { data: node } of graph.nodes) {
     // Escape label
-    const label = escapeLabel(_label);
+    const labelStr = escapeLabel(node.label);
 
     // Only include ID if it's not the same as the label
-    let id = "";
-    if (_id !== _label) {
-      id = `#${_id}`;
+    let idStr = "";
+    if (node.id !== node.label) {
+      idStr = `#${node.id}`;
     }
 
     // Only include classes if there are any
-    let classes = "";
-    if (_classes)
-      classes = _classes
+    let classesStr = "";
+    if (node.classes)
+      classesStr = node.classes
         .split(" ")
         .map((c) => `.${c}`)
         .join("");
 
     // Only include data if there is any
-    const data = stringifyData(_data);
+    const data = stringifyData(node);
 
-    const features = [id, classes, data].filter(Boolean).join("");
-    const line = [label, features].filter(Boolean).join(" ");
+    const features = [idStr, classesStr, data].filter(Boolean).join("");
+    const line = [labelStr, features].filter(Boolean).join(" ");
     lines.push(line);
 
     // get all edges whose source is this node
-    const edges = graph.edges.filter((edge) => edge.source === _id);
-    for (const edge of edges) {
+    const edges = graph.edges.filter((edge) => edge.source === node.id);
+    for (const { target, data: edge } of edges) {
       // get target node
-      const target = graph.nodes.find((node) => node.data.id === edge.target);
-      if (!target) continue;
+      const targetNode = graph.nodes.find((node) => node.data.id === target);
+      if (!targetNode) continue;
 
-      const { label: edgeLabel, id: edgeId, classes: edgeClasses, ...edgeData } = edge.data;
+      let label = escapeLabel(edge.label);
 
-      let label = escapeLabel(edgeLabel);
-
-      let id = edgeId;
-      if (id && id !== edgeLabel) {
-        id = `#${edgeId}`;
+      let id = edge.id;
+      if (id && id !== edge.label) {
+        id = `#${edge.id}`;
       }
 
       let classes = "";
-      if (edgeClasses) {
-        classes = edgeClasses
+      if (edge.classes) {
+        classes = edge.classes
           .split(" ")
           .map((c) => `.${c}`)
           .join("");
       }
 
-      const data = stringifyData(edgeData);
+      const data = stringifyData(edge);
 
       const features = [id, classes, data].filter(Boolean).join("");
       label = [features, label].filter(Boolean).join(" ");
@@ -65,7 +76,10 @@ export function stringify(graph: Graph) {
       if (label) label = `${label}: `;
 
       // link by id, if id is not the same as label, else label
-      const link = target.data.id !== target.data.label ? `#${target.data.id}` : target.data.label;
+      const link =
+        targetNode.data.id !== targetNode.data.label
+          ? `#${targetNode.data.id}`
+          : targetNode.data.label;
 
       // wrap link in ()
       const wrappedLink = `(${link})`;
@@ -86,11 +100,13 @@ export function stringify(graph: Graph) {
 export function stringifyData(data: Record<string, string | number | boolean>) {
   return Object.entries(data)
     .map(([key, value]) => {
+      if (["id", "label", "classes"].includes(key)) return "";
       if (typeof value === "boolean") return `[${key}]`;
       if (typeof value === "number") return `[${key}=${value}]`;
       if (typeof value === "string") return `[${key}="${value.replace(/"/g, '\\"')}"]`;
       throw new Error(`Invalid data type for property "${key}": ${typeof value}`);
     })
+    .filter(Boolean)
     .join("");
 }
 
