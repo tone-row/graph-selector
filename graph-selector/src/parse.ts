40f7372dd3670eadcebf1be7ce0b41c5cf2e4021
Rob Gordon <rob.gordon@tone-row.com>
2024-07-27 11:33:41 -0400

    Switch to bun

--- graph-selector/src/parse.ts
+++ graph-selector/src/parse.ts
@@ -32,7 +32,7 @@ export function parse(text: string): Graph {
   let lines = strip(text, { preserveNewlines: true }).split(/\n/g);
 
   // unescape backslashes in newlines
-  lines = lines.map((line) => line.replace(/\\n/g, "\n"));
+  lines = lines.map((line: string) => line.replace(/\\n/g, "\n"));
 
   // start line number count
   let lineNumber = 0;
